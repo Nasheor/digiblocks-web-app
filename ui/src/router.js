013f4170f961e53788a4d802bdc3ca6125733644
nasheor <nash.avinashnagarajan@gmail.com>
2020-06-12 09:21:34 +0100

    Minor changes to file structure

--- ui/src/router.js
+++ ui/src/router.js
@@ -20,39 +20,6 @@ export default new Router({
           path: '',
           component: Dashboard,
         },
-        // Pages
-        // {
-        //   name: 'User Profile',
-        //   path: 'pages/user',
-        //   component: () => import('@/views/pages/UserProfile'),
-        // },
-        // {
-        //   name: 'Notifications',
-        //   path: 'components/notifications',
-        //   component: () => import('@/views/dashboard/component/Notifications'),
-        // },
-        // {
-        //   name: 'Icons',
-        //   path: 'components/icons',
-        //   component: () => import('@/views/dashboard/component/Icons'),
-        // },
-        // {
-        //   name: 'Typography',
-        //   path: 'components/typography',
-        //   component: () => import('@/views/dashboard/component/Typography'),
-        // },
-        // // Tables
-        // {
-        //   name: 'Regular Tables',
-        //   path: 'tables/regular-tables',
-        //   component: () => import('@/views/dashboard/tables/RegularTables'),
-        // },
-        // // Maps
-        // {
-        //   name: 'Google Maps',
-        //   path: 'maps/google-maps',
-        //   component: () => import('@/views/dashboard/maps/GoogleMaps'),
-        // },
       ],
     },
     {
