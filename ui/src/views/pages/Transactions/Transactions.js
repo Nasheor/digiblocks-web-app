565600bd0bf86582214f60b55d470f034e0b39d4
Avinash <nash.avinashnagarajan@gmail.com>
2020-12-09 09:03:30 +0000

    Minor Changes

--- ui/src/views/pages/Transactions/Transactions.js
+++ ui/src/views/pages/Transactions/Transactions.js
@@ -54,8 +54,8 @@ methods: {
                 chainCodeName: "deccontract",
                 channelName: "mychannel",
                 // args: [JSON.stringify(b.dec_id)],
-                //args: JSON.stringify([b.dec_id]),
-                args: "[\"7e15e460-21a0-11eb-a1f6-dba677a2c98e\"]"
+                // args: JSON.stringify([b.dec_id]),
+                // args: "[\"7e15e460-21a0-11eb-a1f6-dba677a2c98e\"]"
                 // args: [b.dec_id]
             },
         }
