6dd3f90e6fcd405f8981b7f2f89fbe122b78b940
Nasheor <nash.avinashnagarajan@gmail.com>
2020-12-17 14:50:32 +0000

    Minor Changes

--- ui/src/service/thingsboard/http.js
+++ ui/src/service/thingsboard/http.js
@@ -5,9 +5,9 @@ import routes from './routes.json'
 import createAuthRefreshInterceptor from 'axios-auth-refresh';
 
 const BASE_HEADERS = {
-    "Content-Type": "application/json",
-    Accept: "application/json"
-  },
+  "Content-Type": "application/json",
+  Accept: "application/json"
+},
   HOST = config.thingsboard.endpoint;
 
 let http = axios.create({
@@ -16,15 +16,15 @@ let http = axios.create({
 });
 
 const refreshAuthLogic = async failedRequest => {
-  const body = {username: config.thingsboard.username, password: config.thingsboard.password};
-  return await axios.post(HOST + routes.base_login_route.name, body, {headers: BASE_HEADERS})
-    .then(async tokenRefreshResponse => {  
-        console.log(tokenRefreshResponse.data);
-        localStorage.setItem('token', tokenRefreshResponse.data.token);
-        failedRequest.response.config.headers['X-Authorization'] = 'Bearer ' + tokenRefreshResponse.data.token;
-        return await Promise.resolve();
-        }).catch(err => {
-              return Promise.reject(err);
+  const body = { username: config.thingsboard.username, password: config.thingsboard.password };
+  return await axios.post(HOST + routes.base_login_route.name, body, { headers: BASE_HEADERS })
+    .then(async tokenRefreshResponse => {
+      console.log(tokenRefreshResponse.data);
+      localStorage.setItem('token', tokenRefreshResponse.data.token);
+      failedRequest.response.config.headers['X-Authorization'] = 'Bearer ' + tokenRefreshResponse.data.token;
+      return await Promise.resolve();
+    }).catch(err => {
+      return Promise.reject(err);
     });
 };
 /**
@@ -36,11 +36,11 @@ createAuthRefreshInterceptor(http, refreshAuthLogic);
  */
 http.interceptors.request.use(function (config) {
 
-    const token = localStorage.getItem('token');
-    if (!_.isNil(token))
-      config.headers['X-Authorization'] = `Bearer ${token}`;
-    return config;
-  },
+  const token = localStorage.getItem('token');
+  if (!_.isNil(token))
+    config.headers['X-Authorization'] = `Bearer ${token}`;
+  return config;
+},
   (error) => {
     let message = 'ThingsBoard has return status code ' + error.response.status;
     log.log('error', 'error');
